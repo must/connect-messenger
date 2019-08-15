204afbabe2beba6aa42d2dd03b05dbc543d88bf6
Mustapha Ben Chaaben <mustapha3892@gmail.com>
2019-08-15 19:37:59 +0200

    Minor naming changes from options to config in connection.js to better reflect the purpose of the variable

--- connection.js
+++ connection.js
@@ -1,16 +1,17 @@
 const platform = require('connect-platform');
 const Messenger = require('messenger-node');
 
-let options = platform.config.get('messenger', {});
+let config = platform.config.get('messenger', {});
 
 const messengerAPI = {
   _client: undefined,
   get client() {
     if(this._client == undefined) {
-      this._client = new Messenger.Client(options.credentials);
+      this._client = new Messenger.Client(config.credentials);
     }
     return this._client;
   }
 };
 
 module.exports = messengerAPI
+module.exports.config = config
